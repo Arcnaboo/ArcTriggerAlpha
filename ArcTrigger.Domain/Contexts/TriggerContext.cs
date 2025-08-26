fa8f38651ed6cf6fa83e35f89d94a301a50ac15a
Arcnaboo <aakgur@gmail.com>
2025-08-26 10:14:03 +0300

    firstghfghffgdfgd###

---
+++ ArcTrigger.Domain/Contexts/TriggerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace ArcTrigger.Domain.Contexts
+{
+    public class TriggerContext : DbContext
+    {
+    }
+}
